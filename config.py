936ff6dda137c67c033705d34944f2bfcfa6169f
jassbots <157136102+jassbots@users.noreply.github.com>
2024-03-31 21:35:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/THEMADMAXPRO/MadmaxXMusic",
+    "https://github.com/jassbots/MadmaxXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "MadmaxXMusic")
 GIT_TOKEN = getenv(
